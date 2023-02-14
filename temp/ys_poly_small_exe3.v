c83f80835e7f9eaa8ddcda467f3bb8f272af0291
wxkhturf <wxkhturf@163.com>
2023-02-14 22:18:32 +0800

    Update README.md for 'assign'

--- temp/ys_poly_small_exe3.v
+++ temp/ys_poly_small_exe3.v
@@ -10,33 +10,33 @@
       g->coeffs[0] = -(3*g->coeffs[0]);
 
 */
-
 `timescale 1 ns/1 ps
 `include "param.v"
 `include "ys_poly_small.vh"
 module ys_poly_small_exe3(
     
-    f_ctr                        ,                            
-    ram1_douta                   ,                            
-    ram1_doutb                   ,                            
-//OUTPUT 
-    ram2_dina                    ,                            
+    clk                         ,                           
+    f_ctr                       ,                           
+    ram1_douta                  ,                           
+    ram1_doutb                  ,//OUTPUT 
+                               
+    ram2_dina                   ,                           
     ram2_dinb      )
           
     ;
-                         
 //INPUT
+
 input                       clk        ;
 input                         f_ctr      ;
 input   [`DW_PH-1:0]        ram1_douta ;
-input   [`DW_PH-1:0]        ram1_doutb ;
-//OUTPUT
+input   [`DW_PH-1:0]        ram1_doutb ;//OUTPUT
+
 output  [`DW_PH-1:0]          ram2_dina  ;
 output  [`DW_PH-1:0]        ram2_dinb  ;
-
 //REG
-reg     [`DW_13-1:0]        data_dly1  ;
-//WIRE
+
+reg     [`DW_13-1:0]        data_dly1  ;//WIRE
+
 wire   [`DW_PH-1:0]           tmp_dina  ;
 wire    [`DW_PH-1:0]        tmp_dinb  ;
 
@@ -58,7 +58,7 @@ assign tmp_dinb[`DW_13*1 +: `DW_13]  = ram1_doutb[`DW_13*0 +: `DW_13] - ram1_dou
 assign tmp_dinb[`DW_13*2 +: `DW_13]  = ram1_doutb[`DW_13*1 +: `DW_13] - ram1_doutb[`DW_13*2 +: `DW_13] ;
 assign tmp_dinb[`DW_13*3 +: `DW_13]  = ram1_doutb[`DW_13*2 +: `DW_13] - ram1_doutb[`DW_13*3 +: `DW_13] ;
 
-assign ram2_dina[`DW_13*0 +: `DW_13]  = f_ctr ? {tmp_dina[`DW_13*0 +: `DW_13] , 1'b0} + tmp_dina[`DW_13*0 +: `DW_13] 
+assign ram2_dina[`DW_13*0 +: `DW_13]  = f_ctr ? {tmp_dina[`DW_13*0 +: `DW_13] , 1'b0} + tmp_dina[`DW_13*0 +: `DW_13]
 : {tmp , 1'b0} + tmp                                             ;
 assign ram2_dina[`DW_13*1 +: `DW_13]  = {tmp_dina[`DW_13*1 +: `DW_13] , 1'b0} + tmp_dina[`DW_13*1 +: `DW_13] ;
 assign ram2_dina[`DW_13*2 +: `DW_13]  = {tmp_dina[`DW_13*2 +: `DW_13] , 1'b0} + tmp_dina[`DW_13*2 +: `DW_13] ;
